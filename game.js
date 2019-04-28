9aaffe152e6b7bf984fb17756b109faa28b376a6
vonHupfburg <csabasteiber@gmail.com>
2019-04-28 14:01:11 +0200

    refactor: var names
    tileObject is now htmlElement, tile is now Card

--- game.js
+++ game.js
@@ -1,24 +1,23 @@
-class Tile{
+class Card{
   constructor(locX, locY){
     this.locX = locX;
     this.locY = locY;
-    this.tileObject = this.createTile();
-    this.realignTile();
+    this.htmlElement = this.createCard();
+    this.realignCard();
   };
 
-  createTile(){
+  createCard(){
     var tempTile = document.createElement("div");
     tempTile.className = "tile";
     htmlGrid.appendChild(tempTile);
     return tempTile;
   }
 
-  realignTile(){
-    this.tileObject.style.left = this.locX + "px";
-    this.tileObject.style.top = this.locY + "px";
+  realignCard(){
+      this.htmlElement.style.left = this.locX + "px";
+      this.htmlElement.style.top = this.locY + "px";
   }
-
-};
+}
 
 class Grid {
   constructor(numRows, numCols) {
@@ -27,6 +26,7 @@ class Grid {
     this.tileMatrix = this.createTileMatrix();
     console.log(this.tileMatrix);
     console.log(this.tileMatrix[3][4]);
+    this.tileMatrix[1][1].hideTile();
   }
 
   createTileMatrix(){
@@ -34,7 +34,7 @@ class Grid {
     for(var indexRows = 0; indexRows < this.numRows; indexRows++){
       var tempColArray = [];
       for(var indexColumns = 0; indexColumns < this.numCols; indexColumns++){
-        tempColArray.push(new Tile(60 + 60 * indexRows, 100 + 60 * indexColumns));
+        tempColArray.push(new Card(60 + 60 * indexRows, 100 + 60 * indexColumns));
       }
       tempTileMatrix.push(tempColArray);
     }
