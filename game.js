6bbb3067d372a052cd0f85e6e63b4db64bf8b6ec
vonHupfburg <csabasteiber@gmail.com>
2019-05-04 22:55:25 +0200

    feat: add turn & disappear func

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
 class Card{
   constructor(locX, locY, content){
+    // PROPS
     this.locX = locX;
     this.locY = locY;
     this.content = content
     this.htmlElement = this.createCard();
     this.imgElement = this.addImage();
-    //this.hideImage(false);
+    this.isRevealed = false;
+    // Init card
+    this.hideImage(false);
     this.realignCard();
-    //console.log(this.imgElement);
+    // Clickable
+    this.htmlElement.addEventListener("click", this.onClick.bind(this));
+    // CANDY
+    this.animationQueue = [];
+    this.animationBusy = false;
+    this.animationProgress = 0;
   };
 
   createCard(){
@@ -29,8 +37,148 @@ class Card{
     return tempContent
   }
 
+  onClick(){
+    if (this.isRevealed === false){
+      this.showImage(true);
+      grid.rememberReveal(this);
+    }
+  }
+
   hideImage(wantAnimation){
-    this.imgElement.style.display = "none";
+    this.isRevealed = false;
+    console.log("ZZZZ");
+    if (wantAnimation === true){
+      this.queueAnimation("hide");
+    } else {
+      this.imgElement.style.display = "none";
+    }
+  }
+
+  showImage(wantAnimation){
+    this.imgElement.style.display = "block";
+    this.isRevealed = true;
+    if (wantAnimation === true){
+      this.queueAnimation("show");
+    }
+  }
+
+  hideHtmlElement(wantAnimation){
+    console.log("BBBBB");
+    if (wantAnimation === true){
+      this.queueAnimation("dismantle")
+    } else {
+      this.htmlElement.style.display = "none";
+    }
+  }
+
+  queueAnimation(whichAnimation){
+    this.animationQueue.push(whichAnimation);
+    this.runAnimations();
+  }
+
+  runAnimations(){
+    console.log(this.animationQueue);
+    if (this.animationBusy === false && this.animationQueue.length !== 0){
+      this.runNextAnimation();
+    }
+  }
+
+  runNextAnimation(){
+    this.animationBusy = true;
+    this.animationProgress = 0;
+    if (this.animationQueue[0] === "show"){
+      this.runAnimationShow();
+    }
+    if (this.animationQueue[0] === "hide"){
+      this.runAnimationHide();
+    }
+    if (this.animationQueue[0] === "wait"){
+      this.runAnimationWait();
+    }
+    if (this.animationQueue[0] === "dismantle"){
+      this.runAnimationDismantle();
+    }
+  }
+
+  runAnimationShow(){
+    if (this.animationProgress < 100){
+      window.setTimeout(this.runAnimationShow.bind(this), 1);
+      this.animationProgress = this.animationProgress + 2;
+      if (this.animationProgress <= 40){
+        this.htmlElement.style.left = this.locX + (50/40) * this.animationProgress + "px";
+        this.htmlElement.style.width = (100 - 2.5 * this.animationProgress) + "px";
+        this.imgElement.style.display = "none";
+      }
+      if (this.animationProgress > 60){
+        this.htmlElement.style.left = this.locX + 50 - (50/40) * (this.animationProgress - 60) + "px"
+        this.htmlElement.style.width = (-150 + 2.5 * this.animationProgress) + "px";
+        this.imgElement.style.left = 50 + "px";
+        this.imgElement.style.width = (-150 + 2.5 * this.animationProgress) + "px";
+        this.imgElement.style.display = "block";
+      }
+    } else {
+      this.animationProgress = 0;
+      this.animationBusy = false;
+      this.animationQueue.splice(0, 1);
+      this.runAnimations();
+    }
+  }
+
+  runAnimationHide(){
+    if (this.animationProgress < 100){
+      window.setTimeout(this.runAnimationHide.bind(this), 1);
+      this.animationProgress = this.animationProgress + 2;
+      if (this.animationProgress > 60){
+        this.htmlElement.style.left = this.locX + (50/40) * (100 - this.animationProgress) + "px";
+        this.htmlElement.style.width = (100 - 2.5 * (100 - this.animationProgress)) + "px";
+        this.imgElement.style.display = "none";
+      }
+      if (this.animationProgress <= 40){
+        this.htmlElement.style.left = this.locX + 50 - (50/40) * ((100 - this.animationProgress) - 60) + "px"
+        this.htmlElement.style.width = (-150 + 2.5 * (100 - this.animationProgress)) + "px";
+        this.imgElement.style.left = 50 + "px";
+        this.imgElement.style.width = (-150 + 2.5 * (100 - this.animationProgress)) + "px";
+        this.imgElement.style.display = "block";
+      }
+    } else {
+      this.animationProgress = 0;
+      this.animationBusy = false;
+      this.animationQueue.splice(0, 1);
+      this.runAnimations();
+    }
+  }
+
+  runAnimationWait(){
+    if (this.animationProgress < 100){
+      window.setTimeout(this.runAnimationWait.bind(this), 1);
+      this.animationProgress = this.animationProgress + 2;
+    } else {
+      this.animationProgress = 0;
+      this.animationBusy = false;
+      this.animationQueue.splice(0, 1);
+      this.runAnimations();
+    }
+  }
+
+  runAnimationDismantle(){
+    if (this.animationProgress < 100){
+      window.setTimeout(this.runAnimationDismantle.bind(this), 2);
+      this.animationProgress = this.animationProgress + 0.5;
+      this.imgElement.style.width = (100 - this.animationProgress) + "px";
+      this.imgElement.style.height = (100 - this.animationProgress) + "px";
+      this.imgElement.style.left = 50 + "px";
+      this.imgElement.style.top = 50 + "px";
+      this.htmlElement.style.width = (100 - this.animationProgress) + "px";
+      this.htmlElement.style.height = (100 - this.animationProgress) + "px";
+      this.htmlElement.style.top = this.locY + 0.5 * this.animationProgress + "px"
+      this.htmlElement.style.left = this.locX + 0.5 * this.animationProgress + "px"
+    } else {
+      this.animationProgress = 0;
+      this.animationBusy = false;
+      this.animationQueue = []
+      this.htmlElement.style.display = "none";
+      this.imgElement.style.display = "none";
+    }
   }
 }
 
@@ -40,9 +188,47 @@ class Grid {
     this.numRows = this.defineNumRows();
     this.distributionArray = this.createDistributionArray();
     this.cardArray = this.createCardArray();
+    this.RevealedArray = [];
+  }
+
+  checkRevealMaximum(){
+    if (this.RevealedArray.length === 2){
+      for (var index = 0; index < 2; index++){
+        this.RevealedArray[index].hideImage(true);
+      }
+      this.RevealedArray = [];
+    }
+  }
+
+  checkIsEqual(){
+    if (this.RevealedArray[0].content.handle === this.RevealedArray[1].content.handle){
+      console.log("yay you");
+      console.log(this.RevealedArray[0].animationQueue);
+      console.log(this.RevealedArray[1].animationQueue);
+      while (this.RevealedArray[0].animationQueue.length < this.RevealedArray[1].animationQueue.length){
+        console.log("a")
+        this.RevealedArray[0].queueAnimation("wait");
+      }
+      while (this.RevealedArray[1].animationQueue.length < this.RevealedArray[0].animationQueue.length){
+        console.log("b")
+        this.RevealedArray[1].queueAnimation("wait");
+      }
+      this.RevealedArray[0].hideHtmlElement(true);
+      this.RevealedArray[1].hideHtmlElement(true);
+      this.RevealedArray = [];
+    }
+  }
+
+  rememberReveal(whichCard){
+    this.checkRevealMaximum();
+    this.RevealedArray.push(whichCard);
+    if (this.RevealedArray.length === 2){
+      this.checkIsEqual();
+    }
   }
 
   defineNumRows(){
+    console.log(this.cardTypeArray.length);
     return Math.round(Math.sqrt(2 * this.cardTypeArray.length));
   }
 
@@ -115,6 +301,7 @@ class CardType {
 }
 
 var globalCardTypeArray = []
+
 globalCardTypeArray.push(new CardType("images/seal.jpg", "seal"));
 globalCardTypeArray.push(new CardType("images/cow.jpg", "cow"));
 globalCardTypeArray.push(new CardType("images/cat.jpg", "cat"));
@@ -125,6 +312,7 @@ globalCardTypeArray.push(new CardType("images/dog.jpg", "dog"));
 globalCardTypeArray.push(new CardType("images/piglet.jpg", "piglet"));
 globalCardTypeArray.push(new CardType("images/monkey.jpg", "monkey"));
 globalCardTypeArray.push(new CardType("images/whale.jpg", "whale"));
+globalCardTypeArray.push(new CardType("images/rat.jpg", "rat"));
 
 var htmlGrid = document.getElementById('htmlGrid');
 var grid = new Grid(globalCardTypeArray);
